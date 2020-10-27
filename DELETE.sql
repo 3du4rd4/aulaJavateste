7057a908ef3190b17cb366fa9f1d53f8ed804325
Eduarda Oliveira Filadelfo <eolivefi@everis.com>
2020-10-27 14:39:07 -0300

    Aula SQL

---
+++ DELETE.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+Aluno (
+	Matricula,
+	Nome,
+	Idade,
+	DataNascimento
+ ) VALUES (
+		'6598',
+		'Thaynara',
+		25,
+		'1995-12-12'
+)
---
+++ UPDATE.sql
@@ -0,0 +1,4 @@
+UPDATE Aluno SET
+	Idade = 20,
+	Nome = 'Milena'
+WHERE Idade = 20
